448a239283f70d964d13cc97756c1531c2a8c471
Tausheef Ali <tausheefali299@gmail.com>
2026-01-13 10:20:20 +0530

    File Updated

---
+++ app.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click",function(){
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+    console.log("Color updated")
+    let div = document.querySelector(".box");
+    div.style.backgroundColor= randomColor;
+});
+
+function getRandomColor(){
+ let red =Math.floor(Math.random()*255);
+ let green =Math.floor(Math.random()*255);
+ let blue =Math.floor(Math.random()*255);
+ let color = `rgb(${red},${blue},${green})`
+ return color;
+}
